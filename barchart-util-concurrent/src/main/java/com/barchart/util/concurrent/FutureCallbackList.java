f3f4ad2a7e56bc0b8e00b995471dd55a17bfa50f
jjongsma <jeremy@barchart.com>
2012-09-13 17:58:56 -0500

    Added FutureCallback interface support

--- barchart-util-concurrent/src/main/java/com/barchart/util/concurrent/FutureCallbackList.java
+++ barchart-util-concurrent/src/main/java/com/barchart/util/concurrent/FutureCallbackList.java
@@ -8,22 +8,24 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /**
- * <p>
  * Groups a number of {@link FutureCallbackTask} results together, and executes
  * a single callback only after all FutureCallbacks have completed (succeeded or
  * failed.)
- * </p>
+ * 
+ * This class does not support direct access to succeed() or fail(), and will
+ * throw and UnsupportedOperationException for them.
  * 
  * @author jeremy
  * @see FutureCallbackTask
  * @param <E>
  *            The deferred result type
  */
-public class FutureCallbackList<E> implements Future<List<E>> {
+public class FutureCallbackList<E> implements FutureCallback<List<E>> {
 
 	private final FutureListener<E> listener = new FutureListener<E>() {
 
-		public void resultAvailable(Future<E> result_) {
+		@Override
+		public void resultAvailable(final Future<E> result_) {
 			onResponse(result_);
 		}
 
@@ -44,85 +46,110 @@ public class FutureCallbackList<E> implements Future<List<E>> {
 	 * @param deferreds_
 	 *            The list of deferred results to monitor
 	 */
-	public FutureCallbackList(List<? extends FutureCallback<E>> results_) {
+	public FutureCallbackList(final List<? extends FutureCallback<E>> results_) {
 		results = results_;
-		if (results_ == null || results_.size() == 0)
+		if (results_ == null || results_.size() == 0) {
 			fired = true;
-		else {
+		} else {
 			// Lock the semaphore until a result is available
 			resultMonitor.acquireUninterruptibly();
-			for (FutureCallback<E> r : results_)
+			for (final FutureCallback<E> r : results_) {
 				r.addResultListener(listener);
+			}
 		}
 	}
 
 	/**
-	 * Add a callback listener that will be called when all FutureCallbacks
-	 * have completed. The callback will be passed a list of results
-	 * collected from the FutureCallbacks. A null member may indicate that
-	 * a FutureCallback failed, but does not guarantee it (since a
-	 * FutureCallback
-	 * callback could return a null value on purpose.)
+	 * Add a callback listener that will be called when all FutureCallbacks have
+	 * completed. The callback will be passed a list of results collected from
+	 * the FutureCallbacks. A null member may indicate that a FutureCallback
+	 * failed, but does not guarantee it (since a FutureCallback callback could
+	 * return a null value on purpose.)
 	 * 
 	 * @param callback
 	 *            The object to notify of the deferred results
 	 */
-	public void addResultListener(FutureListener<List<E>> callback) {
+	@Override
+	public FutureCallback<List<E>> addResultListener(
+			final FutureListener<List<E>> callback) {
 		listeners.add(callback);
-		if (fired)
+		if (fired) {
 			callback.resultAvailable(this);
+		}
+		return this;
 	}
 
 	/**
 	 * Register a response from a FutureCallback.
 	 */
-	private void onResponse(Future<E> result) {
-		if (fired)
+	private void onResponse(final Future<E> result) {
+		if (fired) {
 			return;
+		}
 		completed++;
 		if (completed == results.size()) {
-			for (FutureCallback<E> r : results) {
+			for (final FutureCallback<E> r : results) {
 				try {
 					responses.add(r.get());
-				} catch (Exception e) {
+				} catch (final Exception e) {
 					responses.add(null);
 				}
 			}
 			fired = true;
 			resultMonitor.release();
-			for (FutureListener<List<E>> l : listeners)
+			for (final FutureListener<List<E>> l : listeners) {
 				l.resultAvailable(this);
+			}
 		}
 	}
 
-	public boolean cancel(boolean interrupt) {
-		if (fired)
+	@Override
+	public boolean cancel(final boolean interrupt) {
+		if (fired) {
 			return false;
+		}
 		cancelled = true;
-		for (FutureCallback<E> r : results)
+		for (final FutureCallback<E> r : results) {
 			r.cancel(interrupt);
+		}
 		return true;
 	}
 
+	@Override
 	public List<E> get() {
-		if (!fired)
+		if (!fired) {
 			resultMonitor.acquireUninterruptibly();
+		}
 		return responses;
 	}
 
-	public List<E> get(long timeout, TimeUnit unit)
+	@Override
+	public List<E> get(final long timeout, final TimeUnit unit)
 			throws InterruptedException, TimeoutException {
-		if (!fired && !resultMonitor.tryAcquire(timeout, unit))
+		if (!fired && !resultMonitor.tryAcquire(timeout, unit)) {
 			throw new TimeoutException("Timed out waiting for result");
+		}
 		return responses;
 	}
 
+	@Override
 	public boolean isCancelled() {
 		return cancelled;
 	}
 
+	@Override
 	public boolean isDone() {
 		return fired;
+	}
+
+	@Override
+	public FutureCallback<List<E>> succeed(final List<E> result) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public FutureCallback<List<E>> fail(final Throwable error) {
+		throw new UnsupportedOperationException();
 	}
 
 }
