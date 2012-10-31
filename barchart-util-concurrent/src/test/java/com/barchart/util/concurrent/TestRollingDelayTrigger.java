21faeffcae346f0c1213c83ce0724d215148175f
jjongsma <jeremy@barchart.com>
2012-10-31 17:27:26 -0500

    Added test project and moved some classes in

--- barchart-util-concurrent/src/test/java/com/barchart/util/concurrent/TestRollingDelayTrigger.java
+++ barchart-util-concurrent/src/test/java/com/barchart/util/concurrent/TestRollingDelayTrigger.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.barchart.util.thread.test.CallableTest;
+import com.barchart.util.test.CallableTest;
 
 public class TestRollingDelayTrigger {
 
--- barchart-util-test/src/main/java/com/barchart/util/test/CallableTest.java
+++ barchart-util-test/src/main/java/com/barchart/util/test/CallableTest.java
@@ -1,4 +1,4 @@
-package com.barchart.util.thread.test;
+package com.barchart.util.test;
 
 import java.lang.reflect.Field;
 import java.util.Collection;
