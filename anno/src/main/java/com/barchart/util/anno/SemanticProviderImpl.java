cb8911b7dd2369836e00b49de1371aa83fce29f8
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-04-05 21:50:12 -0500

    semantic version annotations

---
+++ anno/src/main/java/com/barchart/util/anno/SemanticProviderImpl.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2011-2013 Barchart, Inc. <http://www.barchart.com/>
+ *
+ * All rights reserved. Licensed under the OSI BSD License.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+package com.barchart.util.anno;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * API Provider Implementation.
+ * 
+ * @see https://github.com/barchart/barchart-documents/wiki/Version-Policy
+ */
+@Documented
+@Target({ ElementType.TYPE })
+public @interface SemanticProviderImpl {
+
+	String value() default "";
+
+}
