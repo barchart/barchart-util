b11b77e4cb612bebb216ab9337092c7f9ece0718
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2013-05-14 13:52:51 -0500

    document time

--- value/api/src/main/java/com/barchart/util/value/api/Time.java
+++ value/api/src/main/java/com/barchart/util/value/api/Time.java
@@ -1,23 +1,66 @@
 package com.barchart.util.value.api;
 
+import java.util.TimeZone;
+
 import aQute.bnd.annotation.ProviderType;
 
+import com.barchart.util.anno.NotMutable;
+
+/**
+ * Time value.
+ * <p>
+ * Represented by 2 components:
+ * <li>milliseconds in the UTC time zone
+ * <li>time zone in which to interpret time
+ * <p>
+ * Depending on implementation, periodic time zone update is required for
+ * accurate time zone interpretation.
+ * <li>see <a href="http://joda-time.sourceforge.net/tz_update.html">Joda Time
+ * updater</a>
+ * <li>see <a href=
+ * "http://www.oracle.com/technetwork/java/javase/tzupdater-readme-136440.html"
+ * >JDK updater</a>
+ */
+@NotMutable
 @ProviderType
 public interface Time extends Comparable<Time>, Copyable<Time> {
 
-	String timeZoneCode();
+	/**
+	 * Time zone ID form the TZ database.
+	 * <p>
+	 * Compatible with both JDK and JodaTime.
+	 * <p>
+	 * The return value is guaranteed to be interned, see
+	 * {@link String#intern()}, and can be used for "==" instance equality.
+	 * <p>
+	 * See<a href="https://en.wikipedia.org/wiki/Tz_database">TZ Database</a>
+	 * 
+	 * @see TimeZone
+	 */
+	String zone();
 
 	/**
-	 * milliseconds since January 1, 1970, 00:00:00 GMT
-	 **/
-	long asMillisUTC();
+	 * Milliseconds since January 1, 1970, 00:00:00 GMT
+	 */
+	long millisUTC();
 
+	/**
+	 * Ordering based on {@link Time} based on {@link #millisUTC()}.
+	 */
 	@Override
 	int compareTo(Time thatTime);
 
+	/**
+	 * TODO ensure we can guarantee same hash as JDK 8 / JodaTime.
+	 */
 	@Override
 	int hashCode();
 
+	/**
+	 * Equality of {@link Time} based on {@link #millisUTC()}.
+	 * <p>
+	 * TODO should zone be part of equals?
+	 */
 	@Override
 	boolean equals(Object thatTime);
 
