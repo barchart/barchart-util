ebd5f244d483072e3e19451422d7e9c4a014b0fc
glitchfield <gavin.litchfield@barchart.com>
2013-05-23 13:01:07 -0500

    Revert "Disabled Jackson Conversion, will add if needed"
    This reverts commit 260784a5d55edc28b147b7dff06dd2dfc53dfd3a.

--- values/src/main/java/com/barchart/util/values/json/DecimalValueDes.java
+++ values/src/main/java/com/barchart/util/values/json/DecimalValueDes.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.barchart.util.value.api.Decimal;
-import com.barchart.util.value.api.Scaled;
 import com.barchart.util.values.api.DecimalValue;
 import com.barchart.util.values.provider.ValueBuilder;
 
