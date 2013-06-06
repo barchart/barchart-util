1845aad05c544790e0aeebaa9e83050b9340ffcd
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-06-06 11:59:42 -0500

    hide loader internals

--- value/api/src/main/java/com/barchart/util/value/api/FactoryLoader.java
+++ value/api/src/main/java/com/barchart/util/value/api/FactoryLoader.java
@@ -7,26 +7,30 @@ import com.barchart.util.value.provider.FactoryProvider;
 /**
  * Static service loader.
  */
-public class FactoryLoader extends BaseLoader<Factory> {
+public class FactoryLoader {
+
+	static class Loader extends BaseLoader<Factory> {
+
+		@Override
+		protected Class<? extends Producer<Factory>> providerType() {
+			return FactoryProvider.class;
+		}
+
+		@Override
+		protected Class<Factory> serviceType() {
+			return Factory.class;
+		}
+
+	}
 
 	/**
 	 * Load {@link Factory} from external provider.
 	 */
 	public static Factory load() throws RuntimeException {
-		return new FactoryLoader().produce();
+		return new Loader().produce();
 	}
 
 	private FactoryLoader() {
-	}
-
-	@Override
-	protected Class<? extends Producer<Factory>> providerType() {
-		return FactoryProvider.class;
-	}
-
-	@Override
-	protected Class<Factory> serviceType() {
-		return Factory.class;
 	}
 
 }
