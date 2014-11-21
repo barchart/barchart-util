8c7d44b29a87074917fbbd3140239c02728fb33b
jjongsma <jeremy@barchart.com>
2014-11-21 10:45:58 -0600

    Enable cached reflection collection if available

--- guice/src/main/java/com/barchart/util/guice/AnnotationScanner.java
+++ guice/src/main/java/com/barchart/util/guice/AnnotationScanner.java
@@ -1,9 +1,11 @@
 package com.barchart.util.guice;
 
+import java.net.URL;
 import java.util.Collection;
 import java.util.Set;
 
 import org.reflections.Reflections;
+import org.reflections.util.ClasspathHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,9 +22,15 @@ class AnnotationScanner {
 	private final Set<Class<?>> configuredModuleClasses;
 
 	public AnnotationScanner() {
-		Reflections reflections = new Reflections("");
+
+		final Collection<URL> urls = ClasspathHelper.forPackage("META-INF/reflections/");
+
+		// Load from cache if available (requires dom4j dependency in including project to avoid NoClassDefFound)
+		final Reflections reflections = urls.size() > 0 ? Reflections.collect() : new Reflections("");
+
 		this.componentClasses = ImmutableSet.copyOf(reflections.getTypesAnnotatedWith(Component.class));
 		this.configuredModuleClasses = ImmutableSet.copyOf(reflections.getTypesAnnotatedWith(ConfiguredModule.class));
+
 	}
 
 	public Collection<Class<?>> getComponentClasses() {
