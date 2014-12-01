b29b41e1074efecb81fd06107033d9c1464b37e9
Joel Dudley <joel.dudley@barchart.com>
2014-12-01 10:05:47 -0600

    Guice-util: add default for NetworkInterfaceValueConverter

--- guice/src/main/java/com/barchart/util/guice/valueconverters/NetworkInterfaceValueConverter.java
+++ guice/src/main/java/com/barchart/util/guice/valueconverters/NetworkInterfaceValueConverter.java
@@ -2,10 +2,16 @@ package com.barchart.util.guice.valueconverters;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.TypeLiteral;
 
 public class NetworkInterfaceValueConverter extends ParameterizedValueConverter {
+
+	private static final Logger logger = LoggerFactory.getLogger(NetworkInterfaceValueConverter.class);
 
 	public NetworkInterfaceValueConverter() {
 		super(TypeLiteral.get(NetworkInterface.class), "NetworkInterface");
@@ -13,7 +19,16 @@ public class NetworkInterfaceValueConverter extends ParameterizedValueConverter 
 
 	@Override
 	protected Object convertParameter(String parameter) throws Exception {
-		return NetworkInterface.getByInetAddress(InetAddress.getByName(parameter));
+		try {
+			InetAddress inetAddress = InetAddress.getByName(parameter);
+			return NetworkInterface.getByInetAddress(inetAddress);
+		} catch (UnknownHostException e) {
+			// Not ideal, but if we throw an exception here, than we can't do a verifying build
+			// on machines without the interfaces configured
+			NetworkInterface loopback = NetworkInterface.getByInetAddress(InetAddress.getLoopbackAddress());
+			logger.error("Could not resolve network interface.  Using loopback instead: " + loopback, e);
+			return loopback;
+		}
 	}
 
 }
