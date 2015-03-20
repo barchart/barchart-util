ac4e89d19e3e94823f68d6bf85e02bb68305d300
Yingjie Zhong <yingjie.zhong@barchart.com>
2015-03-20 15:20:14 -0500

    fix long overflow durind round operation

--- value/src/main/java/com/barchart/util/value/impl/BaseScaled.java
+++ value/src/main/java/com/barchart/util/value/impl/BaseScaled.java
@@ -14,7 +14,7 @@ import java.math.BigDecimal;
 import com.barchart.util.value.api.Scaled;
 
 public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
-	
+
 	protected abstract T result(long mantissa, int exponent);
 
 	// yes, same type
@@ -23,11 +23,11 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 	public final T norm() {
 
 		long m = mantissa();
-		
+
 		if(m == 0) {
 			return (T) this;
 		}
-		
+
 		int e = exponent();
 
 		final int p = e;
@@ -68,50 +68,60 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 		return result(m, e);
 
 	}
-	
+
 	@Override
 	public final T round(final int maxSigDigits) {
-		
+
 		if(maxSigDigits < 0) {
 			throw new IllegalArgumentException("Max significant digits must be positive");
 		}
-		
+
 		long m = mantissa();
 		int e = exponent();
-		
+
 		/* If rounding is needed */
 		if(e < 0 && e < (maxSigDigits * -1)) {
-			
+
+			int round = ((maxSigDigits * -1) - e) - 1;
+
+			// long max is 19 digit
+			if (round >= 19) {
+				int i = -(19 + maxSigDigits + e);
+				m = m / (int) Math.pow(10, i);
+				e = e + i;
+				round = ((maxSigDigits * -1) - e) - 1;
+			}
+
 			/* Truncate unused decimals */
-			long powDif = 1; 
-			for(int i = 0; i < (((maxSigDigits * -1) - e) - 1); i++) {
+			long powDif = 1;
+			for (int i = 0; i < round; i++) {
 				powDif = MathExtra.longMult10(powDif);
 			}
 			m /= powDif;
-			
+
 			/* Store 1's digit, determines rounding */
 			long rnd = m - ((m / 10) * 10);
-			
+
 			/* Truncate last decimal */
 			m /= 10;
-			
+
 			/* Increment if rounding up */
 			if(rnd >= 5) {
 				m++;
 			}
-			
+
 			e = maxSigDigits * -1;
 		}
-		
+
 		return result(m, e);
-		
+
 	}
 
 	@Override
 	public final int compareTo(final T that) {
 
 		// TODO How do we handle NULL values?
-		
+
 		int e1 = this.exponent();
 		int e2 = that.exponent();
 
@@ -122,27 +132,27 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 			if ((m1 ^ m2) < 0) { // different sign
 				return m1 >= 0 ? +1 : -1;
 			} else { // identical sign
-				
+
 				while(e1 > 0) {
 					e1--;
 					m1 = MathExtra.longMult10(m1);
 				}
-				
+
 				while(e1 < 0) {
 					e1++;
 					m2 = MathExtra.longMult10(m2);
 				}
-				
+
 				while(e2 > 0) {
 					e2--;
 					m2 = MathExtra.longMult10(m2);
 				}
-				
+
 				while(e2 < 0) {
 					e2++;
 					m1 = MathExtra.longMult10(m1);
 				}
-				
+
 			}
 
 		}
@@ -261,21 +271,21 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 
 	@Override
 	public final T mult(final long factor) throws ArithmeticException {
-		
+
 		return result(MathExtra.longMult(mantissa(), factor), exponent());
 
 	}
-	
+
 	@Override
 	public final T div(final long factor) throws ArithmeticException {
 
 		if(factor == 0) {
 			throw new ArithmeticException("Division by zero");
 		}
-		
+
 		long m = this.mantissa();
 		int e = this.exponent();
-		
+
 		if(m == 0) {
 			return result(m, e);
 		}
@@ -292,12 +302,12 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 		return result(m / factor, e);
 
 	}
-	
+
 	@Override
 	public final T neg() {
 		return result(-mantissa(), exponent());
 	}
-	
+
 	@Override
 	public final T abs() {
 		return result(Math.abs(mantissa()), exponent());
@@ -315,7 +325,7 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 
 	@Override
 	public final T mult(final Scaled<?> that) throws ArithmeticException {
-		
+
 		final int e1 = this.exponent();
 		final int e2 = that.exponent();
 
@@ -325,7 +335,7 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 		return result(MathExtra.longMult(m1, m2), e1 + e2);
 
 	}
-	
+
 	@Override
 	public final T mult(Scaled<?> factor, int maxSigDigits) throws ArithmeticException {
 		return mult(factor).round(maxSigDigits);
@@ -333,13 +343,13 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 
 	@Override
 	public final T div(final Scaled<?> that) throws ArithmeticException {
-		
+
 		int e1 = this.exponent();
 		final int e2 = that.exponent();
 
 		long m1 = this.mantissa();
 		final long m2 = that.mantissa();
-		
+
 		if(m1 == 0) {
 			return result(m1, e1);
 		}
@@ -356,7 +366,7 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 		return result(m1 / m2, e1 - e2);
 
 	}
-	
+
 	@Override
 	public T div(Scaled<?> factor, int maxSigDigits) throws ArithmeticException {
 		return div(factor).round(maxSigDigits);
@@ -370,62 +380,62 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 
 	@Override
 	public T add(long increment) {
-		
+
 		int exO = exponent();
 		int ex = exponent();
 		long ma = mantissa();
-		
+
 		if(ex == 0) {
 			return result(MathExtra.longAdd(mantissa(), increment), ex);
 		} else if(ex > 0) {
-			
+
 			while(ex > 0) {
 				ma = MathExtra.longMult10(ma);
 				ex--;
 			}
-			
+
 			return result(MathExtra.longAdd(ma, increment), 0);
-			
+
 		} else {
-			
+
 			while(ex < 0) {
 				increment = MathExtra.longMult10(increment);
 				ex++;
 			}
-			
+
 			return result(MathExtra.longAdd(ma, increment), exO);
-			
+
 		}
-		
+
 	}
 
 	@Override
 	public T sub(long decrement) {
-		
+
 		int exO = exponent();
 		int ex = exponent();
 		long ma = mantissa();
-		
+
 		if(ex == 0) {
 			return result(MathExtra.longSub(mantissa(), decrement), ex);
 		} else if(ex > 0) {
-			
+
 			while(ex > 0) {
 				ma = MathExtra.longMult10(ma);
 				ex--;
 			}
-			
+
 			return result(MathExtra.longSub(ma, decrement), ex);
-			
+
 		} else {
-			
+
 			while(ex < 0) {
 				decrement = MathExtra.longMult10(decrement);
 				ex++;
 			}
-			
+
 			return result(MathExtra.longSub(ma, decrement), exO);
-			
+
 		}
 	}
 
@@ -438,7 +448,7 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 	public boolean greaterThanOrEquals(final T that) {
 		return this.compareTo(that) >= 0;
 	}
-	
+
 	@Override
 	public boolean lessThan(final T that) {
 		return this.compareTo(that) < 0;
@@ -448,18 +458,18 @@ public abstract class BaseScaled<T extends Scaled<T>> implements Scaled<T> {
 	public boolean lessThanOrEquals(final T that) {
 		return this.compareTo(that) <= 0;
 	}
-	
+
 	@Override
 	public double asDouble() {
-		
+
 		final double result = new BigDecimal(mantissa())
 				.movePointRight(exponent())
 				.doubleValue();
-		
+
 		if(Double.isInfinite(result)) {
 			throw new ArithmeticException("Overflow exception");
 		}
-		
+
 		return result;
 	}
 
