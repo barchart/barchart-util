1038ff3dbdff33baf08ed14579c2ecb52535c1bf
David Lucek <david.lucek@barchart.com>
2015-10-06 11:04:42 -0500

    Added displaying build info

--- guice/src/main/java/com/barchart/util/guice/GuiceLauncher.java
+++ guice/src/main/java/com/barchart/util/guice/GuiceLauncher.java
@@ -1,19 +1,26 @@
 package com.barchart.util.guice;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Joiner;
+import com.google.common.io.Resources;
 import com.google.inject.Injector;
 
 public final class GuiceLauncher {
 
 	private static final Logger logger = LoggerFactory.getLogger(GuiceLauncher.class);
+	private static final String GUICE_APP_MAIN_CLASS = "Guice-App-Main-Class";
 
 	public static <T> Injector buildInjector(final String... args) throws Exception {
 
+		logBuildInfo();
 		logCurrentDirectory();
 		logPaths();
 		logEnvironment();
@@ -38,6 +45,30 @@ public final class GuiceLauncher {
 		}
 
 		return builder.build();
+	}
+
+	private static void logBuildInfo() {
+		try {
+			final URL resource = Resources.getResource("META-INF/MANIFEST.MF");
+			Manifest manifest;
+			manifest = new Manifest(resource.openStream());
+			final Attributes mainAttributes = manifest.getMainAttributes();
+			final String guiceAppMainClass = mainAttributes.getValue(GUICE_APP_MAIN_CLASS);
+			final String artifact = mainAttributes.getValue("Maven-Artifact");
+			final String version = mainAttributes.getValue("Implementation-Version");
+			final String buildDate = mainAttributes.getValue("Build-Date");
+
+			StringBuilder sb = new StringBuilder("Build: ");
+			sb.append("\n\tGuice Main Class: " + guiceAppMainClass);
+			sb.append("\n\tArtifact: " + artifact);
+			sb.append("\n\tVersion: " + version);
+			sb.append("\n\tBuild Date: " + buildDate);
+
+			logger.info(sb.toString());
+
+		} catch (IOException ignore) {
+		}
+
 	}
 
 	public static <T> void run(final Class<T> clazz, final String... args) throws Exception {
